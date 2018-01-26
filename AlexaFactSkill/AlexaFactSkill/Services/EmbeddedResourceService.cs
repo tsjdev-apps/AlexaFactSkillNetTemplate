90f556ba82e6b65fe5912fb41f7b81f922cd15ae
Sebastian Jensen <apps@tsjdev-apps.de>
2018-01-26 14:54:32 +0100

    - add embeddedresource service to read local json file

---
+++ AlexaFactSkill/AlexaFactSkill/Services/EmbeddedResourceService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace AlexaFactSkill.Services
+{
+    public static class EmbeddedResourceService
+    {
+        /// <summary>
+        /// Gets the data from an embedded resource as json file.
+        /// </summary>
+        /// <typeparam name="TClass">The type of the class.</typeparam>
+        /// <param name="resourcePath">The path to the resource.</param>
+        /// <returns>Result of type TClass</returns>
+        public static TClass GetData<TClass>(string resourcePath)
+            where TClass : class
+        {
+            try
+            {
+                var assembly = typeof(EmbeddedResourceService).GetTypeInfo().Assembly;
+                using (var stream = assembly.GetManifestResourceStream(resourcePath))
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var json = reader.ReadToEnd();
+                        return JsonConvert.DeserializeObject<TClass>(json);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"EmbeddedResourceService | GetData | {ex.Message}");
+            }
+
+            return default(TClass);
+        }
+    }
+}
