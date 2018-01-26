b85bcb2d6c3f1740000d3690acce8487d216eddb
Sebastian Jensen <apps@tsjdev-apps.de>
2018-01-26 14:43:09 +0100

    - add extension methods to pick randomly an item

---
+++ AlexaFactSkill/AlexaFactSkill/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlexaFactSkill.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Picks randomly an element of a list.
+        /// </summary>
+        /// <typeparam name="T">The type.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <returns>The random element</returns>
+        public static T PickRandom<T>(this IEnumerable<T> source)
+        {
+            return source.PickRandom(1).Single();
+        }
+
+
+        /// <summary>
+        /// Picks randomly a specified amount of elements of a list.
+        /// </summary>
+        /// <typeparam name="T">The type.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="count">The count.</param>
+        /// <returns>A list of random elements</returns>
+        public static IEnumerable<T> PickRandom<T>(this IEnumerable<T> source, int count)
+        {
+            return source.Shuffle().Take(count);
+        }
+
+        /// <summary>
+        /// Shuffles the specified source.
+        /// </summary>
+        /// <typeparam name="T">The type.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <returns>The shuffled list</returns>
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            return source.OrderBy(x => Guid.NewGuid());
+        }
+    }
+}
