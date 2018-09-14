dcd1c0d8a3d74ed4790a8c7746cd0deb8723a56e
Sebastian Jensen <apps@tsjdev-apps.de>
2018-09-14 07:29:15 +0200

    - add some comments

--- AlexaFactSkill/AlexaFactSkill/Models/Fact.cs
+++ AlexaFactSkill/AlexaFactSkill/Models/Fact.cs
@@ -2,7 +2,14 @@
 {
     public class Fact
     {
+        /// <summary>
+        /// Name of the Intent
+        /// </summary>
         public string Intent { get; set; }
+
+        /// <summary>
+        /// Possible texts as string which are related to the Intent
+        /// </summary>
         public string[] Texts { get; set; }
     }
 }
